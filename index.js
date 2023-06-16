a71e5cf571648fe9f2700832184c41e01a7da78e
michellesambuu <michelle.sambuu@gmail.com>
2023-06-16 07:26:26 -0500

    added function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -53,10 +55,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+   return fs.writeFileSync (path.join(fileName),data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers)=>{
+        writeToFile("README.md",generateMarkdown({...answers}))
+    })
+}
 
 // Function call to initialize app
 init();
