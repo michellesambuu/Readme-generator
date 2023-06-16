92afdf5cd1da932efb4dbbf6efdbe81f7e38cd20
michellesambuu <michelle.sambuu@gmail.com>
2023-06-16 07:37:50 -0500

    added license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,14 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None"){
+    return`* [License](#license)`
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
