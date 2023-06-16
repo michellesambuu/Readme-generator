2fd2c51fcecd7e4a6d7a79c0e77d6276338decdc
michellesambuu <michelle.sambuu@gmail.com>
2023-06-16 07:18:02 -0500

    created input arrays

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"title",
+        message:"what is the title of the project",
+    },
+    {
+        type:"input",
+        name:"description",
+        message:"what is the description of the project",
+    },
+    {
+        type:"input",
+        name:"installation",
+        message:"what are the steps for the installation",
+    },
+    {
+        type:"input",
+        name:"usage",
+    message:"how to use your application",
+    },
+    {
+        type:"list",
+        name:"licence",
+    message:"which license is your project under",
+    choices:["MIT", "ISC", "GNU", "APACHE2.0","None"]
+    },
+    {
+        type:"input",
+        name:"contribution",
+    message:"what are the contributions guidelines",
+    },
+    {
+        type:"input",
+        name:"test",
+    message:"what tests to run",
+    },
+    {
+        type:"input",
+        name:"email",
+    message:"what is your email",
+    },
+    {
+        type:"input",
+        name:"github",
+    message:"what is your github username",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
