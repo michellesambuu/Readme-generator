43f08fa4764572bf90486ce7960c9ffeb8be1bad
michellesambuu <michelle.sambuu@gmail.com>
2023-06-16 07:33:39 -0500

    added render badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  if (license !== "None") {
+    return `<img src="https://img.shields.io/badge/${license}-${license}-green.svg"/>`
+  }
+  else {
+    return ""
+  }
 }
 
 // TODO: Create a function that returns the license link
